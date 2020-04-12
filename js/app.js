bfa59ff6b8cf9408ea9b5e462eb7d469d36ffad8
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-04-11 22:32:47 -0400

    added instructional banner, fix overflow bug when image is out of viewport

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 const MUSIX_KEY = '06b4409f6ef339768789f20421155dad';
 const GIPHY_KEY = "rgXdbqSnr24kKy3JgEjCAFctsgKCR4Zr"
+const MUSIX_API = "https://api.musixmatch.com/ws/1.1/"
+const GIPHY_API = "https://api.giphy.com/v1/gifs/"
 const render_container = document.querySelector(".render-container");
 const COLOR_THEMES = [
     {
@@ -58,12 +60,6 @@ const COLOR_THEMES = [
         secondary: "rgb(220, 196, 150)",
         textColor: "rgb(255, 255, 255)"
     },
-    {
-        name: "deyoung",
-        primary: "rgb(6, 148, 0)",
-        secondary: "rgb(255, 178, 140)",
-        textColor: "rgb(11, 11, 11)"
-    }, // This neeeds to be rgb format.
 ]
 
 /************ INITIAL STATES /************/
@@ -76,17 +72,19 @@ let setInitialState = true; // Variable to capture whether a user first visits o
 /************ SONG/TRACK METHODS ************/ 
 
 // Get Lyrics from MusixMatch API. Returns string
+// Requires CORS to use
 const getLyrics = async (songName, artist) => {
 
     try {
         songName.replace(/\s/g, '%20');
         (artist)? artist.replace(/\s/g, '%20') : null;
-        let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${songName}&q_artist=${artist}&apikey=${MUSIX_KEY}`);
+
+        let response = await fetch(`https://cors-anywhere.herokuapp.com/${MUSIX_API}matcher.lyrics.get?q_track=${songName}&q_artist=${artist}&apikey=${MUSIX_KEY}`);
+
         const search_data = await response.json();
-        console.log(search_data)
         const lyrics_raw = await search_data.message.body.lyrics.lyrics_body;
         const lyrics_clean = lyrics_raw.substring(0, lyrics_raw.indexOf("*"));
-        console.log(lyrics_clean)
+     
         return lyrics_clean;
     }   
     catch(err){
@@ -102,7 +100,8 @@ const getLyrics = async (songName, artist) => {
 const getSongTitleArtistName = async (songName, artist) => {
        /* Render artist, song and album on top */ 
        try {
-            let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/matcher.track.get?q_artist=${artist}&q_track=${songName}&apikey=${MUSIX_KEY}`)
+
+            let response = await fetch(`https://cors-anywhere.herokuapp.com/${MUSIX_API}matcher.track.get?q_artist=${artist}&q_track=${songName}&apikey=${MUSIX_KEY}`)
 
             const track_match = await response.json();
             const track_name = track_match.message.body.track.track_name;
@@ -119,17 +118,20 @@ const getSongTitleArtistName = async (songName, artist) => {
        }
        catch(err){
             console.error(err, "could not find match");
+            document.querySelector("track-info").innerHTML = 
+            `<div class="lyrics-text--error">
+            Could not load track info.<br>
+            </div>`
        }
 }
 
 // Parse Lyrics using Compromise NLP Library
 const parseLyrics = (lyrics_data) => {
-    //const punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+
     let lyrics = nlp(lyrics_data);
 
     // Create a keywords array
     const keywords = [];
-
 
     emptyArr= [];
 
@@ -141,14 +143,6 @@ const parseLyrics = (lyrics_data) => {
     const topics = lyrics.topics().json();
     const adverbs =  lyrics.adverbs().json();
 
-    console.log(verbs);
-    console.log(nouns);
-    console.log(adjectives);
-    console.log(acronyms);
-    console.log(adverbs)
-
-    console.log(keywords);
-
     // Add to keyword list if it's a keyword
     nouns.forEach(noun => { keywords.push(noun.text)})
     verbs.forEach(verb => { keywords.push(verb.text)})
@@ -156,13 +150,11 @@ const parseLyrics = (lyrics_data) => {
     adverbs.forEach(adverb => {keywords.push(adverb.text)})
     topics.forEach(topic => {keywords.push(topic.text)})
 
-    console.log(lyrics.json())
-
     // Split all words into an array based on space 
     parsedLyrics = lyrics_data.split(/ +?/g);
 
     // Make the parsed word into an object if it's a keyword
-    // object contains keyword value (i.e. the giphy search value), html value (string with  <br>) and imgSrc
+    // This object contains a keyword value (i.e. the giphy search value), an html value (string with  <br>) and imgSrc (that will be populated by GIPHY Search)
     for (i = 0; i < parsedLyrics.length ; i++) {
         let htmlString = parsedLyrics[i].replace(/\r\n|\n|\r/gm, "<br>")
         let matchedWord = parsedLyrics[i].split(/(\r\n|\n|\r)/gm)[0];
@@ -179,9 +171,8 @@ const parseLyrics = (lyrics_data) => {
         else {
             parsedLyrics[i] = htmlString;
         }
-    }
-   console.log(parsedLyrics);       
-   
+    }    
+  
    return parsedLyrics.filter(w => w != "");
 }
 
@@ -201,16 +192,24 @@ const searchGif = async (searchTerm) => {
     const selection = Math.floor(Math.random() * 5);
 
     try {
-        let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_KEY}&q=${searchTerm}&offset=${selection}`); // Regular Search
-        //let response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${GIPHY_KEY}&tag=${searchTerm}`); // [ALTERNATIVE] Random Search
+        let response = await fetch(`${GIPHY_API}search?api_key=${GIPHY_KEY}&q=${searchTerm}&offset=${selection}`); // Regular Search
+        
+        /* [ALTERNATIVE] Random Search */
+        //let response = await fetch(`${GIPHY_API}random?api_key=${GIPHY_KEY}&tag=${searchTerm}`);
+
         const giphy_data = await response.json();
-        const gif_url = giphy_data.data[0].images.original.url // Regular Search Query
-        //const gif_url = giphy_data.data.images.original.url; // [ALTERNATIVE] Random Query
+        const gif_url = giphy_data.data[0].images.original.url // Regular Search Parse
+
+        /* [ALTERNATIVE] Random Search Parse */
+        //const gif_url = giphy_data.data.images.original.url; // [ALTERNATIVE] Random GIF Parse
+
         return gif_url
     }   
 
     catch(err){
         console.error(err);
+
+        // Return blank string if no gif exists
         return ""
     }
 }
@@ -245,10 +244,9 @@ const lyricsView = async (list_of_words) => {
     // For each word in the lyrics array, if the word is a key word and there is a gif associated with it, render it onto the page
     for (i = 0; i < list_of_words.length; i++) {
         if (typeof list_of_words[i] === "object" && await list_of_words[i].imgSrc != "") {
-            // If the hml value incldues a break, split the string, putting the key word in the span, and the rest outside of it
+            // If the html value incldues a break, split the string, putting the key word in the <span>, and the rest outside of the <span>
             if (list_of_words[i].htmlVal.includes("<br>")) {
                 let splitString = list_of_words[i].htmlVal.split(/(?=<br>)/g)
-                console.log(splitString)
                 gif_lyrics += `<span class="keyword"><p>${splitString[0]}&nbsp</p><img src="${await list_of_words[i].imgSrc}"></span>
                 ${splitString.slice(1).join("")}&nbsp`
             }
@@ -256,18 +254,15 @@ const lyricsView = async (list_of_words) => {
                 gif_lyrics += `<span class="keyword"><p>${list_of_words[i].htmlVal}&nbsp</p><img src="${await list_of_words[i].imgSrc}"></span>`
             }    
         }
-
         else if (typeof list_of_words[i] === "object" && await list_of_words[i].imgSrc === "") {
             gif_lyrics +=  `${list_of_words[i].htmlVal}&nbsp`
         }
-    
         else {
             gif_lyrics += `${list_of_words[i]}&nbsp`
         }
     }
 
     const html_lyrics = 
-
     `<div class="lyrics-text">
         ${gif_lyrics}
     </div>`
@@ -275,12 +270,33 @@ const lyricsView = async (list_of_words) => {
     return html_lyrics
 }
 
+// Function to correct images out of viewport
+const handleOverflow = (className) => {
+    const windowWidth = window.innerWidth;
+    const elements = document.querySelectorAll(className);
+    elements.forEach(el => {
+        let parent = el.closest("span");
+        parent.addEventListener("mouseover", event => {
+            hideBanner()
+            let rightDistance = 0;
+            let position = el.getBoundingClientRect();
+            if (position.left <= 0) {
+                el.style.left = `${el.style.left.split("px")[0] - el.style.left}px`
+            }
+            rightDistance = windowWidth - position.left - el.width - 2 * el.style.borderWidth.split("px")[0];
+            if (rightDistance < 0) {
+                el.style.left = `${(el.style.left.split("px")[0] - (-rightDistance))}px`
+            }
+        })     
+    })
+}
 
 // Function to switch views to grid mode
 const gridView = async (list_of_words) => {
     document.querySelector(".view-switch").style.background = "blue";
     document.querySelector(".view-switch").style.color = "white";
     document.querySelector(".circle").style.transform = "translateX(50px)";
+    hideBanner();
     let grid_of_gifs = "";
 
     // For each word in the lyrics array, if the word is a key word and there is a gif associated with it, render it onto the page, gif first
@@ -294,13 +310,11 @@ const gridView = async (list_of_words) => {
         }
     }
     const grid = 
-
     `<div class="grid-view">
         ${grid_of_gifs}
     </div>`
     
     return grid
-
 }
 
 const handleViewSwitch = () => {
@@ -309,105 +323,132 @@ const handleViewSwitch = () => {
     })
 }
 
+const showBanner = (text) => {
+    const banner = document.querySelector(".banner")
+    banner.style.transform = "translateX(0px)"
+    banner.style.opacity = "1";
+    banner.innerHTML = text;
+}
+
+const hideBanner = () => {
+    const banner = document.querySelector(".banner")
+    banner.style.transform = "translateX(300px)"
+    banner.style.opacity = "0";
+}
+
 // Renders dynamic elements into html 
 const renderView = async (lyrics) => {
     if (setInitialState) {
         document.querySelector(".view-switch").style.display = "flex";
         render_container.innerHTML = await lyricsView(lyrics)
         assignTheme(COLOR_THEMES);
+        await handleOverflow(".keyword img")
         setInitialState = false;
+        hideConsole();
+        showBanner(`
+            <p class="banner-text">Hover or click <span class="banner-text--emphasis">key words</span> to show GIF!</p>
+            <img src="https://static.thenounproject.com/png/74537-200.png">
+        `);
+        document.querySelectorAll(".keyword p").forEach(word => {
+            word.style.animation = "blink 600ms 2 1.5s"
+        });
     }
-
     else if (gridActive) {
         render_container.innerHTML = await lyricsView(lyrics)
+        gridActive = false;
         assignTheme(CURRENT_THEME);
-        gridActive = false
     }
-
     else {
         render_container.innerHTML = await gridView(lyrics)
-        assignTheme(CURRENT_THEME);
         gridActive = true;
+        assignTheme(CURRENT_THEME);
     }
 }
 
 // Function to change colors randomly upon search, based on pre-set themes
 const assignTheme = async (themes) => {
     const body = document.querySelector("body") // primary
-    const lyrics_text = document.querySelector(".lyrics-text") // black or white
+    const lyrics_text = document.querySelector(".lyrics-text") // text color
     const song_title = document.querySelector(".info-container h1") // secondary
-    const song_artist = document.querySelector(".info-container p") // black or white
+    const song_artist = document.querySelector(".info-container p") // text color
     const keyword_text = document.querySelectorAll(".keyword") // secondary
-
 
     let theme = ""
     
     if (themes.length > 1) {
         let i = Math.floor(Math.random() * COLOR_THEMES.length)
-        theme = themes[i];
+        theme = (themes[i].name === CURRENT_THEME.name) ? themes[i+1] : themes[i];
     }
-
     else {
         theme = themes;
     }     
+    if (!gridActive) {
+        lyrics_text.style.color = theme.textColor;
+        keyword_text.forEach(word => word.style.color = theme.secondary);
+    }
     body.style.backgroundColor = theme.primary;
     song_title.style.color = theme.primary;
     song_title.style['-webkit-text-stroke-color'] = theme.secondary;
     song_artist.style.color = theme.textColor;
-    lyrics_text.style.color = theme.textColor;
-    keyword_text.forEach(word => word.style.color = theme.secondary);
-    console.log(`${theme.name} theme activated...`);
+    
+    /// console.log(`${theme.name} theme activated...`);
     CURRENT_THEME = theme;
 }
 
-
 // Function to show console on user interaction
 const showConsole = () => { 
+
+    const wrapper = document.querySelector(".console-wrapper");
+
     document.querySelector(".console").addEventListener("mouseover", () => {
-        document.querySelector(".console-wrapper").style.transform = "translateX(0px)" 
-        document.querySelector(".console-wrapper").style.transitionDelay = "0s" 
-        document.querySelector(".console-wrapper").style.opacity = "1" 
+        wrapper.style.transform = "translateX(0px)" 
+        wrapper.style.transitionDelay = "0s" 
+        wrapper.style.opacity = "1" 
     })
     document.querySelector(".hover-div").addEventListener("mouseover", () => {
-        document.querySelector(".console-wrapper").style.transform = "translateX(0px)" 
-        document.querySelector(".console-wrapper").style.transitionDelay = "0s" 
-        document.querySelector(".console-wrapper").style.opacity = "1" 
+        wrapper.style.transform = "translateX(0px)" 
+        wrapper.style.transitionDelay = "0s" 
+        wrapper.style.opacity = "1" 
     })
-    document.querySelector(".search-box").addEventListener("mouseover", () => {
-        document.querySelector(".console-wrapper").style.transform = "translateX(0px)" 
-        document.querySelector(".console-wrapper").style.transitionDelay = "0s" 
-        document.querySelector(".console-wrapper").style.opacity = "1" 
+    document.querySelector(".search-inputs").addEventListener("mouseover", () => {
+        wrapper.style.transform = "translateX(0px)" 
+        wrapper.style.transitionDelay = "0s" 
+        wrapper.style.opacity = "1" 
     })
 }
-
 
 // Function to hide console when idle 
 const hideConsole = () => { 
-    document.querySelector(".console-wrapper").style.transform = "translateX(-465px)" 
-    document.querySelector(".console-wrapper").style.opacity = "0.33" 
 
-    document.querySelector(".console").addEventListener("mouseout", () => {
-        document.querySelector(".console-wrapper").style.transform = "translateX(-465px)" 
-        document.querySelector(".console-wrapper").style.transitionDelay = "5s" 
-        document.querySelector(".console-wrapper").style.opacity = "0.33" 
-    })
+    const wrapper = document.querySelector(".console-wrapper");
+    
+    if (!setInitialState && detectWindowLarge() && document.querySelector("input") != document.activeElement) {
+        wrapper.style.transform = "translateX(-465px)" 
+        wrapper.style.opacity = "0.33" 
+   
+        document.querySelector(".console").addEventListener("mouseout", () => {
+        wrapper.style.transform = "translateX(-465px)" 
+        wrapper.style.transitionDelay = "5s" 
+        wrapper.style.opacity = "0.33" 
+        })
 
-    document.querySelector(".hover-div").addEventListener("mouseout", () => {
-        document.querySelector(".console-wrapper").style.transform = "translateX(-465px)" 
-        document.querySelector(".console-wrapper").style.transitionDelay = "5s" 
-        document.querySelector(".console-wrapper").style.opacity = "0.33" 
-    })
-
-
+        document.querySelector(".hover-div").addEventListener("mouseout", () => {
+            wrapper.style.transform = "translateX(-465px)" 
+            wrapper.style.transitionDelay = "5s" 
+            wrapper.style.opacity = "0.33" 
+        })
+    }
 }
 
 // If window is smaller than 768px, don't hide console
-const detectWindow = () => {
+const detectWindowLarge = () => {
     const mediaQuery = window.matchMedia( "(min-width: 768px)" );
     if (mediaQuery.matches) {
         showConsole();
         setTimeout(hideConsole, 5000)
+        return true
     }
+    return false
 }
 
 /************ FORM METHODS ************/ 
@@ -429,9 +470,9 @@ const handleFormSubmit = () => {
 const main = () => {
     handleFormSubmit();
     handleViewSwitch();
-    detectWindow()
+    detectWindowLarge()
 }
 
-main();
-
-
+document.addEventListener("DOMContentLoaded", function(){
+    main();
+});
